bc3bcb6ebe917bff6d3b0511cd05875a23338213
zhuming <zhuming@youshu.cc>
2022-06-26 08:01:29 +0800

    AND-1001 ALI

--- app/src/main/java/com/v/sophixhotfix/MainActivity.java
+++ app/src/main/java/com/v/sophixhotfix/MainActivity.java
@@ -3,12 +3,36 @@ package com.v.sophixhotfix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int a = 0;
+    private boolean isType = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.tv_text);
+        textView.setText(isType ? "我是base包" : "我是patch包");
+
+        findViewById(R.id.btn_click_wrong).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int b = 12;
+                int c = b / a;
+                Log.i("MainActivity", "c:" + c);
+            }
+        });
+
+        findViewById(R.id.btn_click_fix).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //a = 1;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/v/sophixhotfix/MyApplication.java
@@ -0,0 +1,21 @@
+package com.v.sophixhotfix;
+
+import android.app.Application;
+
+/**
+ * ClassName: MyApplication
+ * Description: 自己定义的Application
+ *
+ * @author zhuming
+ * @package_name com.v.sophixhotfix
+ * @date 2022/6/25 23:43
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
